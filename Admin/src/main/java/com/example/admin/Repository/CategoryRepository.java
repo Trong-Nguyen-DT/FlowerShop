7f2af47987b66520b2e36862ffdb02351ddaeefa
phuc_dut <phuckarry634@gmail.com>
2023-10-18 00:17:04 +0700

    testStaff

--- Admin/src/main/java/com/example/admin/Repository/CategoryRepository.java
+++ Admin/src/main/java/com/example/admin/Repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.example.admin.Repository;
 
 import com.example.admin.Entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 }
