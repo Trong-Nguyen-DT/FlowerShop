ffa4abc2a1bb760def45ff5aaf7c356fd8880bb0
Trong-Nguyen-DT <103616323+Trong-Nguyen-DT@users.noreply.github.com>
2023-10-17 22:17:03 +0700

    create and list category done

--- Admin/src/main/java/com/example/admin/Controller/Admin/CategoryController.java
+++ Admin/src/main/java/com/example/admin/Controller/Admin/CategoryController.java
@@ -1,17 +1,38 @@
 package com.example.admin.Controller.Admin;
 
+import com.example.admin.Domain.Category;
+import com.example.admin.Service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("admin")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("category")
     public String listCategory(Model model) {
-//        model.addAttribute("categories", )
+        model.addAttribute("categories", categoryService.getAllCategory());
         return "Admin/CategoryAdmin";
+    }
+
+    @GetMapping("category/add")
+    public String showAddCategory(Model model) {
+        model.addAttribute("category", new Category());
+        return "Admin/AddCategoryAdmin";
+    }
+
+    @PostMapping("category/add")
+    public String addCategory(@ModelAttribute Category category) {
+        categoryService.addCategory(category);
+        return "redirect:/admin/category";
     }
 }
--- Admin/src/main/java/com/example/admin/Service/Impl/CategoryServiceImpl.java
+++ Admin/src/main/java/com/example/admin/Service/Impl/CategoryServiceImpl.java
@@ -1,9 +1,28 @@
 package com.example.admin.Service.Impl;
 
 
+import com.example.admin.Converter.CategoryConverter;
+import com.example.admin.Domain.Category;
+import com.example.admin.Entity.CategoryEntity;
+import com.example.admin.Repository.CategoryRepository;
 import com.example.admin.Service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Override
+    public List<Category> getAllCategory() {
+        return categoryRepository.findAll().stream().map(CategoryConverter::toModel).toList();
+    }
+
+    @Override
+    public void addCategory(Category category) {
+        categoryRepository.save(CategoryConverter.toEntity(category));
+    }
 }
