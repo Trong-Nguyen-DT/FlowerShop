f4f01189659548374641b7072eb103c2be409d48
phuc_dut <phuckarry634@gmail.com>
2023-11-09 09:33:56 +0700

    Admin user(100%)

--- Admin/src/main/java/com/example/admin/Controller/Admin/ItemController.java
+++ Admin/src/main/java/com/example/admin/Controller/Admin/ItemController.java
@@ -12,32 +12,27 @@ import com.example.admin.Service.ItemService;
 public class ItemController {
     @Autowired
     private ItemService itemService;
-
     @GetMapping()
     public String listItem(Model model) {
         model.addAttribute("items", itemService.getAllItem());
         return "Admin/ItemAdmin";
     }
-
     @GetMapping("add")
     public String showAddItem(Model model) {
         model.addAttribute("item", new Item());
         return "Admin/AddItemAdmin";
     }
-
     @PostMapping("add")
     public String addItem(@ModelAttribute Item item) {
         itemService.addItem(item);
         return "redirect:/admin/item";
     }
-
     @GetMapping("edit/{id}")
     public String showEditItem(@PathVariable String id, Model model) {
         Long itemId = Long.parseLong(id);
         model.addAttribute("item", itemService.getItemById(itemId));
         return "Admin/EditItemAdmin";
     }
-
     @PostMapping("edit")
     public String editItem(@ModelAttribute Item item) {
         itemService.updateItem(item);
@@ -56,11 +51,11 @@ public class ItemController {
         model.addAttribute("items", itemService.getAllItem());
         return "Admin/RestoreItemAdmin";
     }
-
     @GetMapping("restore/{id}")
     public String restoreItem(@PathVariable String id) {
         Long itemId = Long.parseLong(id);
         itemService.restoreItemById(itemId);
         return "redirect:/admin/item/restore";
     }
+
 }
