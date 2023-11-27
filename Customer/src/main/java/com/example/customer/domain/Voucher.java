0893cd69ba3d1f29d0cbf14cc85fe186585d18af
Trong-Nguyen-DT <103616323+Trong-Nguyen-DT@users.noreply.github.com>
2023-11-27 14:42:45 +0700

    fix

--- Customer/src/main/java/com/example/customer/domain/Voucher.java
+++ Customer/src/main/java/com/example/customer/domain/Voucher.java
@@ -1,5 +1,6 @@
 package com.example.customer.domain;
 
+import com.example.customer.enums.VoucherType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,11 +11,14 @@ import java.time.LocalDate;
 public class Voucher {
     private Long id;
     private String title;
+    private String icon;
     private String code;
     private double percentage;
     private int usageLimit;
     private LocalDate startDate;
     private LocalDate endDate;
+    private boolean conditionsPaymentOnline;
     private double conditionPrice;
     private boolean expired;
+    private VoucherType type;
 }
