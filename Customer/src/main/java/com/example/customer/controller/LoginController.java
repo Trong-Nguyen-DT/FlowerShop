b793cf81b2fa8509155ef8d4f8df6733f93ea99e
suongchan <minhsuong19102002@gmail.com>
2023-12-27 19:16:11 +0700

    done fix UI

--- Customer/src/main/java/com/example/customer/controller/LoginController.java
+++ Customer/src/main/java/com/example/customer/controller/LoginController.java
@@ -14,7 +14,7 @@ public class LoginController {
     @GetMapping("/login")
     public String loginAdmin(Model model) {
         model.addAttribute("user", new UserEntity());
-        return "Login";
+        return "Login1";
     }
 
 }
